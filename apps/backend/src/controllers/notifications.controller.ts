24d43222c060e51c6f0bd8cf48598658bd63317a
firan-j <97535353+Firan-j@users.noreply.github.com>
2026-09-22 12:24:31 +0300

    feat: create NotificationsController to handle notification-related requests

---
+++ apps/backend/src/controllers/notifications.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+import { notificationService } from '../services/notification.service';
+import { sendSuccess } from '../utils/response';
+
+export class NotificationsController {
+
+    }
+
+export const notificationsController = new NotificationsController();
